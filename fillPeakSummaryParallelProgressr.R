612e8e773cb456bb45b55135d14d901d00d59409
lutiffan <lutiffan@umich.edu>
2024-08-02 17:00:52 -0400

    Added version of fillPeakSummary source code that uses progressr

---
+++ fillPeakSummaryParallelProgressr.R
@@ -0,0 +1,146 @@
+# Requires that hyperparameters exist in the environment
+fillPeakSummaryParallel <- function(betas = NULL) {
+
+  if (is.null(betas)) stop(simpleError("Invalid beta matrix."))
+  
+  template <- data.table("probeName" = character(1),
+                         "numPeaks" = numeric(1),
+                         "meanBeta" = numeric(1),
+                         "peakLocations" = vector(mode = "list", 
+                                                  length = 1),
+                         "leftMin" = vector(mode = "list", 
+                                            length = 1),
+                         "rightMin" = vector(mode = "list", 
+                                             length = 1),
+                         "proportionSample" = vector(mode = "list", 
+                                                     length = 1),
+                         "peakVariance" = vector(mode = "list", 
+                                                 length = 1))
+  
+  # Make new cluster
+  cl <- parallelly::makeClusterPSOCK(availableCores(omit = 1), autoStop = TRUE)
+  
+  # Ensure the cluster is stopped even if the function exits because of an error
+  on.exit(stopCluster(cl), add = TRUE)
+  
+  # Register parallel backend
+  doParallel::registerDoParallel(cl)
+  
+  # .export = ls(envir = globalenv())
+  
+  # # Use progressr::withProgressShiny for Shiny progress bar
+  # progressr::withProgressShiny(message = "Running MethylModes...", detail = "Starting...", expr = {
+  #   
+  #   # Setting up progressr
+  #   p <- progressr::progressor(along = 1:nrow(betas))
+  #   
+  #   peakSummary <- foreach::foreach(idx = 1:nrow(betas), 
+  #                                   .combine = "rbind",
+  #                                   .packages = c("foreach", "data.table"),
+  #                                   .export = c("methylModes",
+  #                                               "localMinMax",
+  #                                               "proportionSample",
+  #                                               "peakDistance",
+  #                                               "kernelType",
+  #                                               "bandwidthType",
+  #                                               "numBreaks",
+  #                                               "densityAdjust",
+  #                                               "pushToZero")) %dopar% {
+  #                                                 probe <- betas[idx, ]
+  #                                                 
+  #                                                 # Steps 1-4: smooth histogram, detect local maxima/minima, filter by spacing, filter by sample %, detect presence of any "gaps"
+  #                                                 foundPeaks <- methylModes(row.data = as.vector(probe))
+  #                                                 detected <- foundPeaks$detected
+  #                                                 probeDensityEst <- foundPeaks$probeDensityEst
+  #                                                 
+  #                                                 # Step 5: Calculate summary statistics
+  #                                                 # 5a: variance for observations included in each peak
+  #                                                 peakVariance = numeric(nrow(detected))
+  #                                                 for (k in 1:nrow(detected)) {
+  #                                                   leftBeta <- probeDensityEst$x[detected$leftMinIdx[k]]
+  #                                                   rightBeta <- probeDensityEst$x[detected$rightMinIdx[k]]
+  #                                                   peakVariance[k] <- var(probe[((probe > leftBeta) & (probe < rightBeta))])
+  #                                                 }
+  #                                                 
+  #                                                 # 5b: mean beta value among all samples
+  #                                                 meanBeta <- mean(probe)
+  #                                                 
+  #                                                 # Fill template
+  #                                                 result <- template[1,]
+  #                                                 result[1,] <- list(
+  #                                                   "probeName" = rownames(betas)[idx],
+  #                                                   "numPeaks" = nrow(detected), 
+  #                                                   "meanBeta" = meanBeta,
+  #                                                   "peakLocations" = list(probeDensityEst$x[detected$maximaIdx]),
+  #                                                   "leftMin" = list(probeDensityEst$x[detected$leftMinIdx]),
+  #                                                   "rightMin" = list(probeDensityEst$x[detected$rightMinIdx]),
+  #                                                   "proportionSample" = list(detected$propSample),
+  #                                                   "peakVariance" = list(peakVariance)
+  #                                                 )
+  #                                                 
+  #                                                 # Update progress
+  #                                                 p()
+  #                                                 
+  #                                                 result
+  #                                               }
+  #   
+  #   peakSummary
+  # })
+  
+  betaIterator <- iter(betas, by = "row")
+  withProgressShiny(message = "Running MethylModes...",
+                    detail = "Starting...",
+                    value = 0, {
+                      progressTracker <- progressor(along = betaIterator)
+                      peakSummary <- foreach::foreach(probe = betaIterator,
+                                                      .combine = "rbind",
+                                                      .packages = c("foreach", "data.table"),
+                                                      .export = c("methylModes",
+                                                                  "localMinMax",
+                                                                  "proportionSample",
+                                                                  "peakDistance",
+                                                                  "kernelType",
+                                                                  "bandwidthType",
+                                                                  "numBreaks",
+                                                                  "densityAdjust",
+                                                                  "pushToZero")) %dopar% {
+
+                                                                    # Steps 1-4: smooth histogram, detect local maxima/minima, filter by spacing, filter by sample %, detect presence of any "gaps"
+                                                                    foundPeaks <- methylModes(row.data = probe)
+                                                                    detected <- foundPeaks$detected
+                                                                    probeDensityEst <- foundPeaks$probeDensityEst
+
+                                                                    # Step 5: Calculate summary statistics
+                                                                    # 5a: variance for observations included in each peak
+                                                                    peakVariance = numeric(nrow(detected))
+                                                                    for (k in 1:nrow(detected)) {
+                                                                      leftBeta <- probeDensityEst$x[detected$leftMinIdx[k]]
+                                                                      rightBeta <- probeDensityEst$x[detected$rightMinIdx[k]]
+                                                                      peakVariance[k] <- var(probe[((probe > leftBeta) & (probe < rightBeta))])
+                                                                    }
+
+                                                                    # 5b: mean beta value among all samples
+                                                                    meanBeta <- mean(probe)
+
+                                                                    template[1,] <- list("probeName" = rownames(probe),
+                                                                                         "numPeaks" = nrow(detected),
+                                                                                         "meanBeta" = meanBeta,
+                                                                                         "peakLocations" = probeDensityEst$x[detected$maximaIdx],
+                                                                                         # "fittedHeights" = probeDensityEst$y[detected$maximaIdx],
+                                                                                         "leftMin" = probeDensityEst$x[detected$leftMinIdx],
+                                                                                         "rightMin" = probeDensityEst$x[detected$rightMinIdx],
+                                                                                         "proportionSample" = detected$propSample,
+                                                                                         "peakVariance" = peakVariance) #,
+                                                                    # "gapFound" = foundPeaks$gap)
+                                                                    # The complicated version of methylModes has foundPeaks$gapFound
+
+                                                                    # Update progress tracker message
+                                                                    #progressTracker(probe)
+                                                                    progressTracker()
+
+                                                                    template
+                                                                  }
+                    })
+  
+  peakSummary
+}
